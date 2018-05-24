01f1a18ad65aac5c0bc891cf4b6eabb7772aa5c8
Kian <kian.noctor@gmail.com>
2018-05-25 02:50:06 +1000

    adding queries to ajax

--- routes/kianajax.js
+++ routes/kianajax.js
@@ -1,7 +1,9 @@
-exports.kianajax = function(req, res, next){
+exports.kianajax = function(req, res){
     //console.log(req.session.userId);
    var user =  req.session.user
+   var friend = req.query.q;
    userID = req.session.userId;
+   console.log(friend);
 
    //Quuery for friends data
     var sqlFriendInfo="SELECT users.id AS id, users.fname as fname, users.lname AS lname FROM users JOIN friends ON users.id = friends.userB WHERE friends.userA=" +userID;
