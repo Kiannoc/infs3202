45e708a806caa4da85356ec20a00c64eb7fc12ac
lukestringer <37282428+lukestringer@users.noreply.github.com>
2018-05-25 11:44:30 +1000

    trying to handle errors, not working yet, how to pass?

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -47,71 +47,107 @@ exports.dashboard = function(req, res, next){
 
 
 //-----------SHOUT FORM PROCESSING -----------//
-exports.shout = function(req, res, next){
+exports.shout = function(req, res){
   userID = req.session.userId;
-  errmessage = '';
+  if(req.method != "POST"){
+    //errmessage = '';
 
-  // Collect field form data
-  var post  = req.body;
-  toProcess = [
-    receiver = post.receiver,
-    amount = post.amount,
-    percentage = post.percentage,
-    description = post.description
-  ];
+    // Collect field form data
+    var post  = req.body;
+    toProcess = [
+      receiver = post.receiver,
+      amount = post.amount,
+      percentage = post.percentage,
+      description = post.description
+    ];
 
-  // trim field form data
-  for(i = 0; i < toProcess.length; i++) {
-    toProcess[i] = toProcess[i].trim();
-    //toProcess[i] = toProcess[i].toLowerCase();
+    // trim field form data
+    for(i = 0; i < toProcess.length; i++) {
+      toProcess[i] = toProcess[i].trim();
+      //toProcess[i] = toProcess[i].toLowerCase();
+    }
+
+    //LOGIC: booleans - field form data is valid
+    var receiverValid = validator.isLength(receiver, {min: 1})
+                    && validator.isAlpha(receiver);
+    var amountValid = validator.isLength(amount, {min: 1})
+                  && validator.isNumeric(amount)
+                  && (validator.isFloat(amount, {min: 0.01})
+                      || validator.isInt(amount, {min: 0}));
+    var percentageValid = validator.isLength(percentage, {min: 1})
+                      && validator.isNumeric(percentage)
+                      && (validator.isFloat(percentage, {min: 0.01, max: 100.00})
+                          || validator.isInt(percentage, {min: 1, max: 100}));
+    var descriptionValid = validator.isLength(description, {min: 3})
+                      && validator.isAlphanumeric(description);
+    //all fields
+    var fieldsValid = receiverValid && amountValid && percentageValid
+                      && descriptionValid;
+
+    // Check if all entered fields are valid
+    if(fieldsValid) {
+      console.log("fields are valid");
+      errmessage = '';
+      // check if receiver is a friend
+      // Query: Match user and Receiver as Friends by ID
+      var sqlConfirmedReceiever = "SELECT userB FROM `friends` WHERE `userA` =  '" + userID +"' AND `userB` = (SELECT id FROM users WHERE `fname`='" + receiver + "')";
+      db.query(sqlConfirmedReceiever, function(err, receiverID){
+        //check if the receiever is confirmed to have a friend with receiverID
+        if(!err && receiverID) {
+          console.log("friend confirmed");
+          //Insert new shout from data
+          console.log(userID);
+          console.log(receiverID);
+          console.log(description);
+          console.log(percentage);
+          console.log(amount);
+
+          var sqlInsertShout = "INSERT INTO `shout`(`buyer`, `receiver`,`description`,`price`, `percentage`,`date`) VALUES ('" + userID + "', '" + receiverID + "','" + description + "','" + amount + "', '" + percentage + "', CURDATE())";
+          db.query(sqlInsertShout, function(err, results) {
+            if(!err) {
+              console.log("query inserted");
+              //res.redirect('/dashboard');
+            }
+          });
+        }
+      });
+    } else {
+      res.render('dashboard.ejs',{errmessage: "ERROR"});
+      /*Field data not valid
+      errorMessages = [
+        recInv = "Receiver is not a valid selection.";
+        amtInv = "Amount is not a valid selection.";
+        percInv = "Percentage is not a valid selection.";
+        descInv = "Description is not a valid selection (Minimum 3 characters).";
+      ];
+      var msgErrorCheckPair = {
+        recInv: receiverValid,
+        amtInv: amountValid,
+        percInv: percentageValid,
+        descInv: descriptionValid
+      };
+      for (i = 0; i < errorMessages.length; i++) {
+        msg = errorMessages[i];
+        if(msgErrorCheckPair.msg) {
+          res.render('dashboard.ejs',{errmessage: msg});
+          //exit loop
+          i = errorMessages.length;
+
+        }
+      }
+      */
+    }
+  } else {
+    //If request is not POST
+    var userID = req.session.userId;
+    if(userID != null){
+        res.redirect('/dashboard');
+    }
+   res.render('dashboard.ejs',{errmessage: errmessage});
   }
 
-  //LOGIC: booleans - field form data is valid
-  var receiverValid = validator.isLength(receiver, {min: 1})
-                  && validator.isAlpha(receiver);
-  var amountValid = validator.isLength(amount, {min: 1})
-                && validator.isNumeric(amount)
-                && (validator.isFloat(amount, {min: 0.01})
-                    || validator.isInt(amount, {min: 0}));
-  var percentageValid = validator.isLength(percentage, {min: 1})
-                    && validator.isNumeric(percentage)
-                    && (validator.isFloat(percentage, {min: 0.01, max: 100.00})
-                        || validator.isInt(percentage, {min: 1, max: 100}));
-  var descriptionValid = validator.isLength(description, {min: 3})
-                    && validator.isAlpha(description);
-  //all fields
-  var fieldsValid = receiverValid && amountValid && percentageValid
-                    && descriptionValid;
 
-  // Check if all entered fields are valid
-  if(fieldsValid) {
-    console.log("fields are valid");
-    // check if receiver is a friend
-    // Query: Match user and Receiver as Friends by ID
-    var sqlConfirmedReceiever = "SELECT userB FROM `friends` WHERE `userA` =  '" + userID +"' AND `userB` = (SELECT id FROM users WHERE `fname`='" + receiver + "')";
-    db.query(sqlConfirmedReceiever, function(err, receiverID){
-      //check if the receiever is confirmed to have a friend with receiverID
-      
-      if(!err && receiverID) {
-        console.log("friend confirmed");
-        //Insert new shout from data
-        console.log(userID);
-        console.log(receiverID[0].userB);
-        console.log(description);
-        console.log(percentage);
-        console.log(amount);
-
-        var sqlInsertShout = "INSERT INTO `shout`(`buyer`, `receiver`,`description`,`price`, `percentage`,`date`) VALUES ('" + userID + "', '" + receiverID[0].userB + "','" + description + "','" + amount + "', '" + percentage + "', CURDATE())";
-        db.query(sqlInsertShout, function(err, results) {
-          if(!err) {
-            console.log("query inserted");
-                    res.redirect('/dashboard');
-                  } 
-                });
-              }
-            });
-          } 
-          };
+};
 
 
 
